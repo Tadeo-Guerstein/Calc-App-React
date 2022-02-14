2eee4b1a7b2bf515310829bafb4c31d5c818e208
Tadeo Guerstein <tadeleonino@hotmail.com>
2022-02-14 10:51:41 -0300

    Calculator made with React

---
+++ src/index.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+ReactDOM.render(<App/>, 
+    document.getElementById("root")
+); //este es el punto de inico de la aplicacion React, lo que hace es insertar el h1 en el div que tiene el id root en el html
