664c7853eac0fe5ba1bc81188bcc908b813b5597
Tadeo Guerstein <tadeleonino@hotmail.com>
2022-02-15 10:18:05 -0300

    import of FC and asigning the component App as a funcional component

---
+++ src/App.tsx
@@ -0,0 +1,54 @@
+/* eslint no-eval: 0 */
+import React, { useState, FC } from 'react';
+import words from 'lodash.words';
+import Functions from './components/Functions'
+import MathOperations from './components/MathOperations'; //*los componentes se importan antes del CSS
+import Numbers from './components/Numbers';
+import Result from './components/Result';
+import './App.css'; //* el css importado de esta manera se utiliza de manera global a todos los componentes
+
+
+// generación de la funcion flecha del componente
+const App: FC = () =>{//* es buena práctica que el nombre de la funcián se llame igual que el archivo
+    
+    // 1er posición: valor (que inicialmente es el valor por defecto)
+    // 2da posicion: funcion que me va a permitir modificar el valor por defecto
+    // [xxxx], [setxxxx]
+    const [stack, setStack] = useState("");
+
+    const items = words(stack, /[^-^+^*^/]+/g)
+    const value = items.length > 0 ? items[items.length-1] : "0";
+    // Es similar a un if
+    // esVerdadero ? (resultadoPorVerdadero) : (resultadoPorFalso)
+
+    const onClickOperation = operation => {
+        setStack(`${stack}${operation}`);
+    };
+    const onClickEqual = equal => {
+        setStack(eval(stack).toString());
+    };
+    const onClickNumbers = number => {
+        setStack(`${stack}${number}`);
+    };
+    const onContentClear = () => {
+        setStack("");
+    };
+    const onDelete = () => {
+        if(stack.length > 0 ){
+            const newStack = stack.substring(0, stack.length - 1)
+            setStack(newStack)
+        }
+    };
+
+    return(
+        <main className='react-calculator'>
+            <Result value = {value}/>
+            <Numbers onClickNumber={onClickNumbers}/>
+            <Functions onContentClear={onContentClear} onDelete={onDelete}/>
+            <MathOperations onClickOperation={onClickOperation} onClickEqual={onClickEqual}/>
+        </main> //* el elemento main se utiliza para lo principal del documento
+    )
+}  
+
+// exportación
+export default App
